dadc1363108d2c33e284bf722e92e57263a78515
kantapong00 <kantapong_kasemsan@elearning.cmu.ac.th>
2020-04-07 15:52:39 +0700

    Revert "first commit"
    This reverts commit 6db90f3c95ad68d776110c8f05fa3d57f1171f37.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const AppWithRouter = () => (
-  <BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-ReactDOM.render(<AppWithRouter />, document.getElementById('root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/styleComponent/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const BackgroundRed = styled.div`
-  display: flex;
-  background-color: #E63C49;
-  height: 100vh;
-  margin: 0px;
-`
