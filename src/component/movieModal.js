adc614b5c3fb3285817d82d5cb6b748b47fd0dca
kantapong00 <kantapong_kasemsan@elearning.cmu.ac.th>
2020-04-13 15:13:46 +0700

    add like function in modal

--- src/component/movieModal.js
+++ src/component/movieModal.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from 'antd'
 import 'antd/dist/antd.css'
 import MovieItemContext from './movieItemContext'
+import { HeartOutlined, HeartFilled } from '@ant-design/icons'
 
 export default function MovieModal(props) {
+  const [like, setlike] = useState(false)
   const item = React.useContext(MovieItemContext)
+
+  const onLike = () => {
+    setlike(true)
+  }
+
+  const onUnLike = () => {
+    setlike(false)
+  }
 
   return (
     <div>
@@ -12,11 +22,11 @@ export default function MovieModal(props) {
         title={item.title}
         visible={props.visible}
         onCancel={props.onCancel}
-        footer={<div>55555</div>}
+        footer={like ? <HeartFilled style={{ fontSize: '24px', color:'red' }} onClick={onUnLike} /> :
+        <HeartOutlined style={{ fontSize: '24px' }} onClick={onLike} />}
       >
-        <img alt="example" src={item.image_url} style={{width:'100%'}} />
-        <p style={{paddingTop:'16px'}}>{item.overview}</p>
-        <p>{Math.random()}</p>
+        <img alt="bg" src={item.image_url} style={{ width: '100%' }} />
+        <p style={{ paddingTop: '16px' }}>{item.overview}</p>
       </Modal>
     </div>
   )
--- src/page/Main.js
+++ src/page/Main.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, Suspense } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Layout, Menu } from 'antd'
 import 'antd/dist/antd.css'
 import { BodyContent, FooterStyle } from '../component/style'
-import loadingIcon from '../component/loading.gif'
+import loadingIcon from '../component/asset/loading.gif'
 import MovieListContext from '../component/movieListContext'
+import ListItems from '../component/listItem'
 
 export default function Main() {
   const { Header, Content } = Layout
   const axios = require('axios').default
-  const ListItems = React.lazy(() => import('../component/listItem'))
 
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
@@ -33,11 +33,12 @@ export default function Main() {
       </Header>
       <Content style={{ padding: '24px 24px 0px 24px' }}>
         <BodyContent>
-          <Suspense fallback={<img src={loadingIcon} alt="loading" style={{ width: '64px' }} />}>
+          {loading ?
+            <img src={loadingIcon} alt="loading" style={{ width: '64px' }} /> :
             <MovieListContext.Provider value={movies}>
               <ListItems />
             </MovieListContext.Provider>
-          </Suspense>
+          }
         </BodyContent>
       </Content>
       <FooterStyle>My Movies ©2020 Created by Kantapong</FooterStyle>
